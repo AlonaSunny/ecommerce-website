683899a1eb91b71740abd5a28d53210f1a56061d
Alona <99014035+AlonaSunny@users.noreply.github.com>
2024-04-27 23:00:03 +0530

    product

--- models.py
+++ models.py
@@ -1,30 +1,16 @@
 from django.db import models
 
-
 # Create your models here.
-from customers.models import customer
-from product.models import product
-
-# Create your models here.
-class Order(models.Model):
-    live=0
+class product(models.Model):
+    live=1
     delete=0
+    title=models.CharField(max_length=50)
+    price=models.IntegerField()
+    description=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='media')
     deletechoices=((live,'live'),(delete,'delete'))
-    cartstage=1
-    orderconfirmed=1
-    orderrejected=4
-    orderprocessed=2
-    orderdelivered=3
-    statuschoice=((orderprocessed,"orderprocessed"),
-                  (orderdelivered,"orderdelivered"),
-                  (orderrejected,"orderrejected"))
-
-    
-    owner=models.ForeignKey(customer,on_delete=models.SET_NULL,null=True,related_name='orders')
     deletestatus=models.IntegerField(choices=deletechoices,default=live)
     createdat=models.DateTimeField(auto_now_add=True)
-    updatedat=models.DateTimeField(auto_now_add=True)
-    class ordereditem(models.Model):
-        product=models.ForeignKey(product,related_name='added_carts',on_delete=models.SET_NULL,null=True)
-        quantity=models.IntegerField(default=1)
-        owner=models.ForeignKey('order.Order',on_delete=models.CASCADE,related_name='orderitem')
+    updatedat=models.DateTimeField(auto_now=True)
+    def _str_(self):
+        return self.title
