9f71806f7357e7f0d4dae92b6a3e291a2bd64b27
Alona <99014035+AlonaSunny@users.noreply.github.com>
2024-04-27 22:59:13 +0530

    Add files via upload
    orders

--- models.py
+++ models.py
@@ -1,20 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 # Create your models here.
-class customer(models.Model):
+from customers.models import customer
+from product.models import product
+
+# Create your models here.
+class Order(models.Model):
     live=0
     delete=0
     deletechoices=((live,'live'),(delete,'delete'))
-    name=models.CharField(max_length=200)
-    address=models.CharField(max_length=700)
-    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer profile+')
-    phone=models.CharField(max_length=30)
+    cartstage=1
+    orderconfirmed=1
+    orderrejected=4
+    orderprocessed=2
+    orderdelivered=3
+    statuschoice=((orderprocessed,"orderprocessed"),
+                  (orderdelivered,"orderdelivered"),
+                  (orderrejected,"orderrejected"))
+
+    
+    owner=models.ForeignKey(customer,on_delete=models.SET_NULL,null=True,related_name='orders')
     deletestatus=models.IntegerField(choices=deletechoices,default=live)
     createdat=models.DateTimeField(auto_now_add=True)
     updatedat=models.DateTimeField(auto_now_add=True)
-    def _str_(self)->str:
-        return self.title
-        
-
+    class ordereditem(models.Model):
+        product=models.ForeignKey(product,related_name='added_carts',on_delete=models.SET_NULL,null=True)
+        quantity=models.IntegerField(default=1)
+        owner=models.ForeignKey('order.Order',on_delete=models.CASCADE,related_name='orderitem')
